62c0986e0ea66ef55683f3259131b5528d05c249
Josh Lippiner <jlippiner@noctivity.com>
2013-09-27 18:31:08 -0400

    fixed versions and download js

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -6,8 +6,8 @@ class AppController < ApplicationController
     std = AppVersion.standard
     pro = AppVersion.pro
 
-    std_hash = std ? {version: std.version_number, url: std.version_file.url} : {}
-    pro_hash = pro ? {version: pro.version_number, url: pro.version_file.url} : {}
+    std_hash = std ? {version: std.version_number, url: absolute(std.version_file.url)} : {}
+    pro_hash = pro ? {version: pro.version_number, url: absolute(pro.version_file.url)} : {}
 
     render json: {taper: std_hash, taperpro: pro_hash}
   end
@@ -18,5 +18,9 @@ class AppController < ApplicationController
 
   def help
     redirect_to 'http://taper.uservoice.com/knowledgebase'
+  end
+
+  def absolute(url)
+    "#{request.protocol}#{request.host_with_port}#{url}"
   end
 end
