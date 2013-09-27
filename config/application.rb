c88b8d24baa5b67ecfbc852b21212dde9572b9f7
Josh Lippiner <jlippiner@noctivity.com>
2013-09-27 15:33:44 -0400

    moved s3 to production only

--- config/application.rb
+++ config/application.rb
@@ -37,15 +37,5 @@ module Tapermail
       g.helper_specs false
       g.controller_specs false
     end
-
-    config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-        :bucket => CONFIG[:s3_bucket_name],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
-    }
-
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,15 @@ Tapermail::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => CONFIG[:s3_bucket_name],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
+
 end
